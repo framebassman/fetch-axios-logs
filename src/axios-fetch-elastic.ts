ef1c6d13fe9230ec9f13eb5a18f4b5dc70fa8651
Dmitry Romashov <framebassman@gmail.com>
2025-05-03 23:38:17 +0200

    It works

---
+++ src/axios-fetch-elastic.ts
@@ -0,0 +1,39 @@
+import winston from 'winston';
+
+// import { CustomHttpElasticTransport } from './custom-http-elastic-transport';
+// import { Agent } from 'http';
+import { AxiosTransport } from 'winston-fetch-axios';
+import axios from 'axios';
+
+let dateString = new Date(new Date()).toISOString().split('T')[0];
+dateString = dateString.replace('-', '.');
+
+export const log = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.prettyPrint()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    // new CustomHttpElasticTransport({
+    //   ssl: true,
+    //   host: 'kolenka-inc-4135333449.eu-central-1.bonsaisearch.net',
+    //   port: 443,
+    //   auth: {
+    //     username: 'NX4jPVtxmC',
+    //     password: 'QNw5bzyHoXC9YFkr'
+    //   },
+    //   path: `filebeat-7.10.2-${dateString}/_doc/`,
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   }
+    // }),
+    new AxiosTransport({
+      url: 'https://kolenka-inc-4135333449.eu-central-1.bonsaisearch.net:443',
+      path: `filebeat-7.10.2-${dateString}/_doc/`,
+      auth: 'Tlg0alBWdHhtQzpRTnc1Ynp5SG9YQzlZRmty',
+      authType: 'basic'
+    })
+  ]
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hi');
+// import { log } from './winston-elasticsearch-logger';
+import { log } from './axios-fetch-elastic';
+
+export const sayHello = () => {
+  let dateString = new Date(new Date()).toISOString().split('T')[0];
+  dateString = dateString.replace('-', '.');
+  log.info('hello world');
+  log.info(`Time now: ${dateString}`);
+  return 1;
+};
+
+sayHello();
